4a21cd8537493c05b7c2b677c9b5641fe6af90e9
evg_ser1997@mail.ru <evg_ser1997@mail.ru>
2023-02-21 14:50:19 +0300

    2.1

--- Lab2/Example.cpp
+++ Lab2/Example.cpp
@@ -1,5 +1,5 @@
-#include <iostream.h>
-#include <iomanip.h>
+#include <iostream>
+#include <iomanip>
 #include <conio.h>
 #define N 5
 void main()
@@ -7,5 +7,13 @@ void main()
   float m[N][N];
   int i, j;
   for (i = 0; i < N; i++)
-    for
+    for (j = 0; j < N; j++)
+      m[i][j] = rand() / 10.;
+  for (i = 0; i < N; i++)
+  {
+    for (j = -; j < N; j++)
+      count << setw(8) << setprecision(5) << m[i][j];
+    count << endl;
+    getch();
+  }
 }
