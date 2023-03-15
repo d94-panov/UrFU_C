37066aec3ad444e9748d47aaca300e2fa267501b
Dmitriy <d94-panov@mail.ru>
2023-03-15 18:52:12 +0500

    Lab9_Tstack

---
+++ Lab9/Tstack.cpp
@@ -0,0 +1,32 @@
+#include <cstring>
+
+template <class T>
+class Tstack
+{
+protected:
+    int numItem;
+    T *item;
+public:
+    Tstack(size_t size = 10)
+    {
+        numItem = 0;
+        item = new T[size];
+    }
+    ~Tstack()
+    {
+        delete[] item;
+    }
+    void push(T t);
+    T pop();
+};
+
+template <class T>
+void Tstack <T>::push(T t)
+{
+    item[numItem++] = t;
+}
+template <class T>
+T Tstack <T>::pop()
+{
+    return item[--numItem];
+}
